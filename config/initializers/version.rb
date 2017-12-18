4617779a72ce97e1603f8f3eaf725960d9442244
Aleksey <ins_rf@mail.ru>
2017-12-19 00:53:29 +0500

    fix

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,9 @@
-version application 1.1
+module TestGuru
+  MAJOR_VERSION = 0
+  MINOR_VERSION = 1
+  PATCH_VERSION = 1
+
+  def self.version
+    version = [MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION]
+  end
+end
