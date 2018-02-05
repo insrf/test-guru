33630cfbb54a0b0b00e774c31dadfeb83059015a
Aleksey <ins_rf@mail.ru>
2018-02-05 21:31:18 +0500

    add 8 lesson

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,34 +1,54 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index]
+  before_action :find_test , only: %i[index new create]
+  before_action :find_question , only: %i[show edit update destroy]
 
   def index
-    render plain: @test.questions.pluck(:body).join("\n")
-    #render plain: Question.where(test_id: params[:test_id]).pluck('body').join("\n")
+    @test.questions
   end
 
   def show
-    render plain: Question.find(params[:id]).body
   end
 
   def new
+    @question = Question.new
+  end
+
+  def edit
   end
 
   def create
-    question = Question.create(body: question_params[:body], test_id: params[:test_id])
-    render plain: question.inspect
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   def destroy
-    Question.destroy(params[:id])
+    @question.destroy
+    redirect_to test_questions_path(@question.test)
   end
 
   private
 
   def question_params
-    params.require(:question).permit(:body)
+    params.require(:question).permit(:body, :test_id)
   end
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,16 +2,37 @@ class TestsController < ApplicationController
   before_action :find_test, only: %i[show]
 
   def index
-    render plain: Test.pluck('title').join("\n")
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
   end
 
   def show
-    render plain: @test.title
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
   end
 
   private
 
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def find_question
+    @questions = Question.where(test_id: params[:id])
   end
 end
