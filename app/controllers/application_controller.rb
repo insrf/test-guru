fafb6f5324a0a362a66212119f356b76bb70d264
Aleksey <ins_rf@mail.ru>
2018-02-21 21:50:47 +0500

    add name and last_name for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Hey, #{current_user.first_name}"
     if current_user.is_a?(Admin)
       admin_tests_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout}
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout},
+              controllers: { registrations: 'users/registrations' }
 
   resources :tests, only: :index do
     resources :questions, shallow: true, expect: :index do
