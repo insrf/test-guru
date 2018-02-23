ecbcde6cedf39df8ac0cbfe101dd1870a0ee26c0
Aleksey <ins_rf@mail.ru>
2018-02-23 20:02:10 +0500

    add set local method without url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
-  before_action :set_locale
   before_action :authenticate_user!
   before_action :set_locale
 
@@ -19,6 +18,13 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+    if I18n.locale_available?(params[:lang])
+      I18n.locale = params[:lang]
+      session[:locale] = I18n.locale
+    elsif I18n.locale_available?(session[:locale])
+      I18n.locale = session[:locale]
+    else
+      I18n.locale = I18n.default_locale
+    end
   end
 end
