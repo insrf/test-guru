922bfff640a90485b2d7858222f7ceec1dabb717
Aleksey <ins_rf@mail.ru>
2018-02-24 12:19:48 +0500

    corrected method default url option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_locale
 
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
+
+
   def after_sign_in_path_for(user)
     flash[:notice] = "Hey, #{current_user.first_name}"
     if current_user.admin?
@@ -18,13 +23,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    if I18n.locale_available?(params[:lang])
-      I18n.locale = params[:lang]
-      session[:locale] = I18n.locale
-    elsif I18n.locale_available?(session[:locale])
-      I18n.locale = session[:locale]
-    else
-      I18n.locale = I18n.default_locale
-    end
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
