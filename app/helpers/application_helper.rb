a6b468af909b71771c5f04da94c5c701862ca730
Aleksey <ins_rf@mail.ru>
2018-02-25 11:18:22 +0500

    add helper for choose css for flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
+
+  FLASH_KEY = { 'notice' => 'alert alert-success', 'alert' => 'alert alert-danger' }.freeze
+
   def year_now
     Time.current.year
   end
+
+  def flash_css_class(key)
+    FLASH_KEY[key]
+  end
+
 end
