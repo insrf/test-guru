9d9e3c07860d8deef4d9cb034c15256df8746b66
Aleksey <ins_rf@mail.ru>
2018-03-07 16:55:40 +0500

    delete same aploads

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module TestGuru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.autoload_paths << "#{Rails.root}/lib/clients"
   end
 end
