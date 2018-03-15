0f23be2f6e0b0a3a3fea3b77bc1427bf5cfa8077
Aleksey <ins_rf@mail.ru>
2018-03-16 00:48:44 +0500

    add lesson 16

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,11 +1,13 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy start update_inline]
 
   def index
-    @tests = Test.all
+
   end
 
   def show
+
   end
 
   def new
@@ -33,6 +35,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
@@ -42,6 +52,10 @@ class Admin::TestsController < Admin::BaseController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_tests
+    @tests = Test.all
   end
 
   def find_test
