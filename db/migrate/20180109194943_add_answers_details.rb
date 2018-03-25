d9fc1650a3e80863c82a7448443ed834ecfd032b
Aleksey <ins_rf@mail.ru>
2018-03-25 23:15:24 +0500

    add correct type boolean migration

--- db/migrate/20180109194943_add_answers_details.rb
+++ db/migrate/20180109194943_add_answers_details.rb
@@ -1,6 +1,7 @@
 class AddAnswersDetails < ActiveRecord::Migration[5.1]
   def change
     change_column(:answers, :body, :text)
+    change_column(:answers, :correct, :boolean)
     change_column_default(:answers, :correct, 0)
     rename_column(:answers, :test_id, :question_id)
   end
