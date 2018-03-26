f05512d768881f0a03bb89c4ae0f93145518ca88
Aleksey <ins_rf@mail.ru>
2018-03-26 20:10:40 +0500

    delete some nothing doing migrate

--- db/migrate/20180109194943_add_answers_details.rb
+++ db/migrate/20180109194943_add_answers_details.rb
@@ -2,7 +2,7 @@ class AddAnswersDetails < ActiveRecord::Migration[5.1]
   def change
     change_column(:answers, :body, :text)
     change_column(:answers, :correct, :boolean)
-    change_column_default(:answers, :correct, 0)
+    change_column_default(:answers, :correct, false)
     rename_column(:answers, :test_id, :question_id)
   end
 end
