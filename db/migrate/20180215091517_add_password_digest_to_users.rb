0aad1015ef8384881de84e8773154259977abd17
insrf <ins_rf@mail.ru>
2018-04-09 18:36:26 +0500

    work with migrate

--- db/migrate/20180215091517_add_password_digest_to_users.rb
+++ db/migrate/20180215091517_add_password_digest_to_users.rb
@@ -1,7 +1,7 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :password_digest, :string
-    rename_column :users, :login
+    remove_column :users, :login
     remove_column :users, :password
   end
 end
